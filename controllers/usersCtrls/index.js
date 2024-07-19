26afa3d2c5f2bd0f5fe4d142dd15a29a07cd4089
VPresich <valya.alexandrova.presich@gmail.com>
2024-07-19 16:03:20 +0200

    Deleted save avatar function to GCS (also Environment Variables), because is not used in this

--- controllers/usersCtrls/index.js
+++ controllers/usersCtrls/index.js
@@ -2,7 +2,6 @@ import getCurrent from './getCurrent.js';
 
 import getAvatar from './getAvatar.js';
 import getAvatarUrl from './getAvatarUrl.js';
-import updateAvatarGCS from './updateAvatarGCS.js';
 import updateAvatarCloudinary from './updateAvatarCloudinary.js';
 import updateProfile from './updateProfileAvatar.js';
 import updateTheme from './updateTheme.js';
@@ -13,7 +12,6 @@ export default {
   getAvatar,
   getCurrent,
   getAvatarUrl,
-  updateAvatarGCS,
   updateAvatarCloudinary,
   updateProfile,
   updateTheme,
--- helpers/constants.js
+++ helpers/constants.js
@@ -3,12 +3,10 @@ export const PHONE_PATTERN = /^\(\d{3}\) \d{3}-\d{4}$/;
 export const NAME_PATTERN =
   /^[a-zA-Z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?\/\\|`~\-\s]{2,32}$/;
 
-export const CLOUD_STORAGE = 'https://storage.cloud.google.com/';
-export const DEF_THEME = 'light';
+export const DEF_THEME = 'yellow';
 
 const def_avatar = 'defavatar' + DEF_THEME.toLowerCase() + '1x.jpg';
 export const AVATAR_SIZE_1 = 68;
-export const PATH_DEF_AVATAR_CGS = `${CLOUD_STORAGE}${process.env.GOOGLE_BUCKET_NAME}/avatars/${def_avatar}`;
 
 export const PATH_DEF_VIOLET_AVATAR =
   'https://res.cloudinary.com/dirtbd4yk/image/upload/v1717687718/def-avatar-violet_1x_f2b0ft.jpg';
