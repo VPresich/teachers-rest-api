c4a92b53f49d74903e602fdc756bb5038fc8ca05
VPresich <valya.alexandrova.presich@gmail.com>
2025-12-17 01:48:45 +0100

    Change import swagger

--- routes/docs.js
+++ routes/docs.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger.json' assert { type: 'json' };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerPath = path.join(__dirname, '../swagger.json');
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf8'));
+
 const docRouter = express.Router();
 docRouter.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
